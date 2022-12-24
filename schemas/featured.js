1cbb6f1e990cfb81990ccbf5b9658a38ea172d75
mika-127-0-0-1 <mika.sidwell@gmail.com>
2022-12-24 16:01:49 +0200

    first commit

---
+++ schemas/featured.js
@@ -0,0 +1,28 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'featured',
+  title: 'Featured Menu Categories',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Featured catergory name',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'short_description',
+      title: 'Short Description',
+      type: 'string',
+      validation: (Rule) => Rule.max(200),
+    }),
+    defineField({
+      name: 'resturants',
+      title: 'Resturants',
+      type: 'array',
+      of: [{type: "reference", to: [{type: "restaurant"}]}],
+    }),
+  ],
+  
+})
--- schemas/index.js
+++ schemas/index.js
@@ -1,6 +1,6 @@
-import blockContent from './blockContent'
 import category from './category'
-import post from './post'
-import author from './author'
+import featured from './featured'
+import restaurant from './restaurant'
+import dish from './dish'
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [restaurant, category, featured, dish]
